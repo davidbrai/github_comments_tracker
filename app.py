0391158c8479fc56716a2f50331a9c58407a3f6b
davidbrai <davidbrai@gmail.com>
2013-06-04 23:56:50 +0300

    showing first 20 comments, VERY SLOW

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, session, redirect, url_for, jsonify
 from flask_oauthlib.client import OAuth
 import logging
+logging.basicConfig(level=logging.INFO)
 
-logging.basicConfig(level=logging.DEBUG)
+from github import Github
 
 log = logging.getLogger(__name__)
 
@@ -15,7 +16,7 @@ github = oauth.remote_app(
     'github',
     consumer_key=github_app_settings['consumer_key'],
     consumer_secret=github_app_settings['consumer_secret'],
-    request_token_params={'scope': 'user:email'},
+    request_token_params={'scope': 'repo'},
     base_url='https://api.github.com/',
     request_token_url=None,
     access_token_method='POST',
@@ -26,11 +27,19 @@ github = oauth.remote_app(
 @app.route("/")
 def index():
     if not 'github_token' in session:
-        log.debug("redirecting to login")
+        log.info("redirecting to login")
         return redirect(url_for('login'))
 
-    me = github.get('user')
-    return jsonify(me.data)
+    log.info("getting comments from github")
+    github_client = Github(session['github_token'][0])
+    repo = github_client.get_repo(app.config['REPO_TO_FETCH'])
+    comments_text = []
+    comments = repo.get_comments()
+    for i, comment in enumerate(comments[:20]):
+        log.info("at comment: %s" % i)
+        comments_text.append(comment.body)
+    log.info("got: %s", comments_text[:5])
+    return jsonify({'comments': comments_text})
 
 @app.route('/login')
 def login():
@@ -44,7 +53,7 @@ def logout():
 @app.route('/login/authorized')
 @github.authorized_handler
 def authorized(resp):
-    log.debug("authorized!")
+    log.info("authorized!")
 
     if resp is None:
         return 'Access denied: reason=%s error=%s' % (
@@ -52,8 +61,7 @@ def authorized(resp):
             request.args['error_description']
         )
     session['github_token'] = (resp['access_token'], '')
-    me = github.get('user')
-    return jsonify(me.data)
+    return redirect(url_for('index'))
 
 @github.tokengetter
 def get_github_oauth_token():

aced8d9763ad4052f0ba6899f85c613940e635f3
davidbrai <davidbrai@gmail.com>
2013-06-07 00:50:22 +0300

    little refactor in tests

--- tests/dao_test.py
+++ tests/dao_test.py
@@ -18,9 +18,12 @@ class TestDao(unittest.TestCase):
         else:
             raise AssertionError("do you really want to drop database: %s?" % self.mongodb.name)
 
+    def _comment(self, id=123, commit='somecommitid', path='somepath', line='someline'):
+        return {'id': 123, 'commit': commit, 'path': path, 'line': line}
+
     def test_puts_comments_in_same_commit_path_line_in_same_thread(self):
-        comment1 = {'id': 123, 'commit': 'somecommitid', 'path': 'somepath', 'line': 'someline'}
-        comment2 = {'id': 124, 'commit': 'somecommitid', 'path': 'somepath', 'line': 'someline'}
+        comment1 = self._comment(id=123)
+        comment2 = self._comment(id=124)
         
         dao.save_to_thread(comment1)
         dao.save_to_thread(comment2)
@@ -31,8 +34,8 @@ class TestDao(unittest.TestCase):
         self.assertIn(comment2, threads[0]['comments'])
     
     def test_comments_on_different_lines_are_on_different_threads(self):
-        comment1 = {'id': 123, 'commit': 'somecommitid', 'path': 'somepath', 'line': 'someline'}
-        comment2 = {'id': 124, 'commit': 'somecommitid', 'path': 'somepath', 'line': 'differentline'}
+        comment1 = self._comment(id=123, line='someline')
+        comment2 = self._comment(id=124, line='differentline')
         
         dao.save_to_thread(comment1)
         dao.save_to_thread(comment2)
@@ -41,10 +44,12 @@ class TestDao(unittest.TestCase):
         assert len(threads) == 2
     
     def test_doesnt_save_the_same_comment_twice_by_id(self):
-        comment = {'id': 123, 'commit': 'somecommitid', 'path': 'somepath', 'line': 'someline'}
+        comment = self._comment()
         
         dao.save_to_thread(comment)
         dao.save_to_thread(comment)
         
         threads = dao.get_threads()
         self.assertEqual(len(threads[0]['comments']), 1)
+
+

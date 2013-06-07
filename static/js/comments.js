faad33015613712d003ee6b5c455b3c183faa69c
davidbrai <davidbrai@gmail.com>
2013-06-07 18:04:48 +0300

    showing 'from now' in timestamps

--- static/js/comments.js
+++ static/js/comments.js
@@ -7,6 +7,13 @@ var app = angular.module('comments', ['threads']).config(function($routeProvider
     });
 });
 
+angular.module('comments').
+    filter('fromNow', function() {
+        return function(dateString) {
+            return moment(new Date(dateString)).fromNow()
+        };
+    });
+
 function CommentsCtrl($scope, Thread) {
     $scope.threads = Thread.query();
 }

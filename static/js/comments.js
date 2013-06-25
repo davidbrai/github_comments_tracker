d9919364a7af7866f7e34fdfca39e2c36fe3b4f4
davidbrai <davidbrai@gmail.com>
2013-06-26 00:55:10 +0300

    show markdown using showdown lib

--- static/js/comments.js
+++ static/js/comments.js
@@ -9,12 +9,30 @@ var app = angular.module('comments', ['threads']).config(function($routeProvider
     });
 });
 
-angular.module('comments').
-    filter('fromNow', function() {
-        return function(dateString) {
-            return moment(new Date(dateString)).fromNow()
-        };
-    });
+angular.module('comments').filter('fromNow', function() {
+    return function(dateString) {
+        return moment(new Date(dateString)).fromNow()
+    };
+});
+
+angular.module('comments').filter('markdown', function() {
+    var converter = new Showdown.converter();
+    return function(markdownString) {
+        return converter.makeHtml(markdownString);
+    }
+});
+
+angular.module('comments').filter('escapeHtml', function() {
+    return function(text) {
+        if (text) {
+            return text.
+                replace(/&/g, '&amp;').
+                replace(/</g, '&lt;').
+                replace(/>/g, '&gt;');
+        }
+        return '';
+    }
+});
 
 function AllThreadsCtrl($scope, Threads) {
     $scope.mode = 'all';
